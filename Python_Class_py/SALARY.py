264b247e076ba70166a5d7a97f77d86f582f6046
Bappi Singh <bappi.r.singh@gmail.com>
2025-06-24 01:36:30 +0530

    num divide by 5 and 10 completed and salary wala question is reamining

---
+++ Python_Class_py/SALARY.py
@@ -0,0 +1,13 @@
+'''
+
+Hello write a python program to calculate salary of considering sales bonus and intensive offer to him based on total sales if exist greater than equals to one lakh rupees Follow the particular column 1 as follows 2
+column A: 
+Book is 4000 rupees 
+hba equals to 20% of basic
+BA equals to 110% of basic
+Conveyance fee around 500 to be and insist use 110% of sales and bonus is ₹1000
+Column B:
+Books equals to ₹4000
+10% of basic
+
+'''
---
+++ Python_Class_py/num_div_5_or_not.py
@@ -0,0 +1,5 @@
+n = int(input("Enter n:"))
+if n%5 ==0 and n%10 == 0:
+    print("It is divisble by 5 and 10 both")
+else:
+    print("It is not divisible by 5 and 10 both")
