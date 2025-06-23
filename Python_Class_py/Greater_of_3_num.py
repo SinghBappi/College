58452691c60733c28bae593248b03aadf7a62a6e
Bappi Singh <bappi.r.singh@gmail.com>
2025-06-24 01:48:28 +0530

    Gratest of 3 Num

---
+++ Python_Class_py/Greater_of_3_num.py
@@ -0,0 +1,10 @@
+n1 = int(input("Enter n1:"))
+n2 = int(input("Enter n1:"))
+n3 = int(input("Enter n1:"))
+if n1>n2 and n1>n3:
+    print("n1 is greatest!")
+if n2>n1 and n2>n3:
+    print("n2 is greatest!")
+if n3>n1 and n3>n1:
+    print("n3 is greatest!")
+    
