a212d05bb71fedf934f6765d0c56428319ab5bf2
Bappi Singh <bappi.r.singh@gmail.com>
2025-08-01 20:42:40 +0530

    other programs added!

--- Practices/string_1.py
+++ Practices/string_1.py
@@ -43,4 +43,120 @@ str2 = "\t"
 #     print(var[i])
 #     i=i+1
 
+#fibonacci series !
+# def fibo():
+#     n = int(input("Enter a number:"))
+#     a=0
+#     b=1
+#     while(a<n):
+#         print(a)
+#         temp = a
+#         a=b
+#         b=temp+b
+# fibo()
+
+# Stars Started !
+
+
+# for i in range(5,0,-1):
+#     for j in range(i):
+#         print("*",end="")
+#     print()
+#     '''
+#     PS D:\COLLEGE REPO\College\Practices> python -u "d:\COLLEGE REPO\College\Practices\string_1.py"
+# *****
+# ****
+# ***
+# **
+# *
+# PS D:\COLLEGE REPO\College\Practices> 
+#     '''
+    
+
+
+# for i in range(1,5,1):
+#     for j in range(5-i):
+#         print("*")
+# print(" ")
+        
+        
+
+# for i in range(1,5+1):
+#     print("*"*i)
+#     '''
+    
+#     PS D:\COLLEGE REPO\College\Practices> python -u "d:\COLLEGE REPO\College\Practices\string_1.py"
+# *
+# **
+# ***
+# ****
+# *****
+# PS D:\COLLEGE REPO\College\Practices> 
+
+#     '''
+
+
+
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#         print(j,end="")
+#     print()
+# '''
+# PS D:\COLLEGE REPO\College\Practices> python -u "d:\COLLEGE REPO\College\Practices\string_1.py"
+# 1
+# 12
+# 123
+# 1234
+# 12345
+# PS D:\COLLEGE REPO\College\Practices> '''
+
+
+# for i  in range(1,5,1):
+#     for j in range(1,i+1):
+#         print(j,end="")
+#     print()
+
+# for i  in range(3,0,-1):
+#     for j in range(1,i+1):
+#         print(j,end="")
+#     print()
+
+#     '''
+#     PS D:\COLLEGE REPO\College\Practices> python -u "d:\COLLEGE REPO\College\Practices\string_1.py"
+# 1
+# 12
+# 123
+# 1234
+# 123
+# 12
+# 1
+#     '''
+
+
+# n = int(input("Enter the number:"))
+# sum =0
+# copynum = n
+# order = len(str(n))
+# while n>0:
+#     digit = n%10
+#     sum = sum + digit ** order
+#     n=n//10
+# if sum==copynum:
+#     print("it's a armstrong number")
+# else:
+#     print("it's not a armstrong number")
+
+# Palindrome Number
+n = 252
+rev = 0
+copy = n
+while n>0:
+    rev = (rev*10)+(n%10)
+    n=n//10
+
+if copy == rev:
+    print("Palindrome Number!")
+    
+
+
 
