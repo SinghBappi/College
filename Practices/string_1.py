ff9a6886df0b90eb65592457857ace5b83602672
Bappi Singh <bappi.r.singh@gmail.com>
2025-08-01 17:25:01 +0530

    string practices

---
+++ Practices/string_1.py
@@ -0,0 +1,46 @@
+# str1 = "Hello"
+# str2 = " World!"
+# str3 = str1+str2
+# print(str3) String concatenation
+
+# string repetition
+# str1 = "spam"*2
+# print(str1)
+
+#get particular character
+# str1 = "hello test"
+# print(str1[2])
+
+# # Slicing
+# test = "Test String"
+# print(test[0:5])
+# print(test[:3])
+# print(test[2:])
+# print(test[-1])
+# print(test[-1:-11:-1])
+
+#String Methods
+str1 = "Ba\tppi"
+str2 = "\t"
+# print(str1.islower())
+# print(str1.lower())
+# print(str1.isupper())
+# print(str1.upper())
+# print(str1.upper().isupper())
+# print(str1.isalpha())
+# print(str1.isdigit())
+# print(str2.isspace())
+# print(str1.find("1"))
+# print(str1.replace("pp","qq"))
+# print(str1.count("p"))
+# print(len(str1))
+
+
+# var = "banana"
+# i=0
+# # print(var[4:0])
+# while i<len(var):
+#     print(var[i])
+#     i=i+1
+
+
